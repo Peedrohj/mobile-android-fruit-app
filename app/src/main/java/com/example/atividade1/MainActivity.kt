3f0a509986083bbe2f58304c62b329f2a2e2b311
Pedro Farias <phjfarias@hotmail.com>
2021-04-18 17:22:33 -0300

    feat: create base list

--- app/src/main/java/com/example/atividade1/MainActivity.kt
+++ app/src/main/java/com/example/atividade1/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.atividade1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val baseList = generateBaseList(20)
+
+        fruit_list.adapter = FruitAdapter(baseList)
+        fruit_list.layoutManager = LinearLayoutManager(this)
+        fruit_list.setHasFixedSize(true)
+    }
+
+    private fun generateBaseList(size: Int): List<Fruit>{
+        val list = ArrayList<Fruit>()
+
+        for (i in 0 until size){
+            val item = Fruit(0, title="Fruta: $i", description = "Uma descrição qualquer")
+            list += item
+        }
+
+        return  list
     }
 }
